71138781b60da623a5dbc9331050e15b61c801aa
Mounir <mounir.khalifa@esprit.tn>
2024-05-07 17:33:10 +0100

    updated badwords and speech to text

--- EdulanceJava/EdulanceJava/src/main/java/com/example/edulancejava/addCategoryController.java
+++ EdulanceJava/EdulanceJava/src/main/java/com/example/edulancejava/addCategoryController.java
@@ -6,6 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -57,4 +58,13 @@ public class addCategoryController {
         alert.setContentText(message);
         alert.showAndWait();
     }
+
+    /**private void showAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Warning");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }**/
+
 }
